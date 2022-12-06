872313b4c005a068fafa2bb007ac6197e1c7528b
vib3sf <velobaton228@gmail.com>
2022-12-06 17:55:49 +0300

    first commit

---
+++ ToDoList/Models/MainModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList;
+
+public class MainModel
+{
+    private DateTime _currentDate = DateTime.Today;
+
+    public Dictionary<string, TaskModel> TasksDictionary = new();
+
+    private void CheckTasks()
+    {
+        foreach (var task in TasksDictionary.Values)
+        {
+            
+        }
+    }
+    
+    
+    public MainModel()
+    {
+        
+    }
+
+}
---
+++ ToDoList/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDoList;
+
+public class TaskModel
+{
+    public string Task { get; set; }
+    public bool IsDone { get; set; }
+    
+    public DateTime DeadLine { get; set; }
+    
+    public TaskModel(string task)
+    {
+        Task = task;
+        IsDone = false;
+    }
+}
