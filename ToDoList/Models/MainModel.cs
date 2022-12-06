94c1edbb06a4a47121d95a4cf0dc205b0db6ab12
vib3sf <velobaton228@gmail.com>
2022-12-07 02:39:55 +0300

    make add, remove tasks

--- ToDoList/Models/MainModel.cs
+++ ToDoList/Models/MainModel.cs
@@ -1,27 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Prism.Mvvm;
 
 namespace ToDoList;
 
-public class MainModel
+public class MainModel : BindableBase
 {
+    private readonly ObservableCollection<TaskModel> _taskList = new();
+    public Dictionary<string, List<TaskModel>> SortedTaskList { get; private set; }
 
-    public List<TaskModel> TaskList = new();
-
-    public void AddTask(string task, int year, int month, int day)
+    public void AddTask(string task, DateTime createDate)
     {
-        TaskList.Add(new TaskModel(task, new DateTime(year, month, day)));
+        _taskList.Add(new TaskModel(task, createDate));
+        SortedTaskList = SortTask();
+        RaisePropertyChanged("SortedTaskList");
     }
 
     public void AddTask(string task)
     {
-        TaskList.Add(new TaskModel(task));
+        _taskList.Add(new TaskModel(task));
+        SortedTaskList = SortTask();
+        RaisePropertyChanged("SortedTaskList");
     }
 
-    public void RemoveTask(string taskDate, TaskModel task)
+    public void RemoveTask(TaskModel task)
     {
-        TaskList.Remove(task);
+        _taskList.Remove(task);
     }
 
     public Dictionary<string, List<TaskModel>> SortTask()
@@ -31,8 +37,11 @@ public class MainModel
             ["Overdue"] = new(),
             ["Without date"] = new()
         };
-        foreach (var task in TaskList)
+        foreach (var task in _taskList)
         {
+            if (task.IsDone)
+                continue;
+            
             if (task.DeadLine == default)
                 taskDictionary["Without date"].Add(task);
             else if (task.DeadLine < DateTime.Today)
@@ -43,25 +52,16 @@ public class MainModel
                 taskDictionary[task.DeadLine.ToShortDateString()] = new List<TaskModel> { task };
 
         }
-
+        RaisePropertyChanged("SortedTaskList");
         return taskDictionary;
     }
 
     public MainModel()
     {
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", new DateTime(2005, 3, 4)));
-        TaskList.Add(new TaskModel("покакать"));
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", new DateTime(2005, 3, 4)));
-        TaskList.Add(new TaskModel("покакать"));
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", DateTime.Today));
-        TaskList.Add(new TaskModel("почесать жопу", new DateTime(2005, 3, 4)));
-        TaskList.Add(new TaskModel("покакать"));
-
+        _taskList.Add(new TaskModel("почесать жопу", DateTime.Today));
+        _taskList.Add(new TaskModel("почесать жопу", DateTime.Today));
+        _taskList.Add(new TaskModel("почесать жопу", new DateTime(2225, 3, 4)));
+        SortedTaskList = SortTask();
     }
 
 }
--- ToDoList/ViewModel/MainViewModel.cs
+++ ToDoList/ViewModel/MainViewModel.cs
@@ -1,9 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace ToDoList.ViewModel;
 
-public class MainViewModel
+public class MainViewModel : BindableBase
 {
     private MainModel _mainModel = new();
-    public Dictionary<string, List<TaskModel>> SortedTaskDictionary => _mainModel.SortTask();
+    public Dictionary<string, List<TaskModel>> SortedTaskDictionary { get; set; }
+    public DelegateCommand CreateCommand { get; }
+    
+    public string TaskTextBlock { get; set; }
+    public DateTime? CreateTaskDate { get; set; }
+
+    public DelegateCommand RemoveCommand { get; set; }
+    
+    public TaskModel SelectedTask { get; set; }
+
+    public MainViewModel()
+    {
+        _mainModel.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+        SortedTaskDictionary = _mainModel.SortedTaskList;
+        
+        CreateCommand = new DelegateCommand(() =>
+        {
+
+            if (CreateTaskDate == null)
+                _mainModel.AddTask(TaskTextBlock!);
+            else
+                _mainModel.AddTask(TaskTextBlock!, (DateTime)CreateTaskDate!);
+            
+            SortedTaskDictionary = _mainModel.SortedTaskList;
+            RaisePropertyChanged("SortedTaskDictionary");
+        });
+        RemoveCommand = new DelegateCommand(() =>
+        {
+            if (SelectedTask != null) _mainModel.RemoveTask(SelectedTask);
+        });
+    }
 }
