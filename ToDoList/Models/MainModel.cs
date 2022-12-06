d77c484dc6363fb7be1d8e4fbb43eda059fbbc6c
vib3sf <velobaton228@gmail.com>
2022-12-06 18:30:39 +0300

    main model core

--- ToDoList/Models/MainModel.cs
+++ ToDoList/Models/MainModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList;
 
@@ -7,20 +8,42 @@ public class MainModel
 {
     private DateTime _currentDate = DateTime.Today;
 
-    public Dictionary<string, TaskModel> TasksDictionary = new();
+    public Dictionary<string, List<TaskModel>> TasksDictionary = new();
 
-    private void CheckTasks()
+    private void CheckOverdueTasks()
     {
-        foreach (var task in TasksDictionary.Values)
+        TasksDictionary["overdue"] = new List<TaskModel>();
+        foreach (var taskDate in TasksDictionary.Keys
+                     .Where(taskDate => DateTime.Parse(taskDate) < _currentDate))
         {
-            
+            TasksDictionary["overdue"].AddRange(TasksDictionary[taskDate]);
+            TasksDictionary.Remove(taskDate);
         }
     }
-    
+
+    public void AddTask(string task, int year, int month, int day)
+    {
+        var taskModel = new TaskModel(task, new DateTime(year, month, day));
+        var strDate = taskModel.DeadLine.ToShortDateString();
+        if (TasksDictionary.ContainsKey(strDate))
+            TasksDictionary[strDate].Add(taskModel);
+        else
+            TasksDictionary[strDate] = new List<TaskModel> { taskModel };
+    }
+
+    public void AddTaskWithoutDate(string task)
+    {
+        TasksDictionary["Without date"].Add(new TaskModel(task));
+    }
+
+    public void RemoveTask(string taskDate, TaskModel task)
+    {
+        TasksDictionary[taskDate].Remove(task);
+    }
     
     public MainModel()
     {
-        
+        CheckOverdueTasks();
     }
 
 }
--- ToDoList/Models/TaskModel.cs
+++ ToDoList/Models/TaskModel.cs
@@ -6,12 +6,17 @@ public class TaskModel
 {
     public string Task { get; set; }
     public bool IsDone { get; set; }
-    
+
     public DateTime DeadLine { get; set; }
-    
+
     public TaskModel(string task)
     {
         Task = task;
         IsDone = false;
+    }
+    
+    public TaskModel(string task, DateTime deadLine) : this(task)
+    {
+        DeadLine = deadLine;
     }
 }
